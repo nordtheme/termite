81a5a8711af4cbba3f7a65e8c5497a71af638a16
Arctic Ice Studio <development@arcticicestudio.com>
2018-01-04 11:28:09 +0100

    Migrate to MIT license
    Adapted to the MIT license migration of the main "Nord" (1) project.
    Detailed information can be found in the main task ticket.
    
    All license SVG badges and the "LICENSE.md" content have been updated.
    In addition all source code comment header have also been simplified.
    
    References:
    
    * (1) https://github.com/arcticicestudio/nord
    
    Subtask of arcticicestudio/nord#55
    GH-7

--- install.sh
+++ install.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-# title      Install Script                                  +
-# project    nord-termite                                    +
-# repository https://github.com/arcticicestudio/nord-termite +
-# author     Arctic Ice Studio                               +
-# email      development@arcticicestudio.com                 +
-# copyright  Copyright (C) 2016                              +
-# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# Copyright (c) 2016-present Arctic Ice Studio <development@arcticicestudio.com>
+# Copyright (c) 2016-present Sven Greb <code@svengreb.de>
+
+# Project:    Nord Termite
+# Repository: https://github.com/arcticicestudio/nord-termite
+# License:    MIT
+
 set -e
 
 _ct_error="\e[0;31m"
